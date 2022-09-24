e11db94241fc9a490b2260594de1968e1cf6cc9e
miguelcmaramara <miguelcmaramara@gmail.com>
2022-09-24 14:48:09 -0400

    styling landing

--- src/App.js
+++ src/App.js
@@ -14,20 +14,6 @@ function App() {
   })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Landing page ={percentage.page} setPercentage={setPercentage}/>
       <Prompt calculator={percentage.calculator} setPercentage={setPercentage}/>
       <Slider min={0} max={24} id={"temp"} calculator = {percentage.calculator} setPercentage={setPercentage}/>
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -3,8 +3,19 @@ import "./css/Landing.css"
 export default function Landing(props){
     return(
         <div id="Landing">
-            <h1 className="title">WeNo</h1>
-            <p className="caption">Help us help you</p>
+            <div className="background"></div>
+            <div className="text before">
+                <div className="center-text">
+                    <h1 className="title">We<span className="green">No</span></h1>
+                    <h3 className="caption">Help us help you</h3>
+                </div>
+            </div>
+            <div className="text after">
+                <div className="center-text">
+                    <h1 className="title">WeNo</h1>
+                    <h3 className="caption">Help us help you</h3>
+                </div>
+            </div>
         </div>
     )
 }
